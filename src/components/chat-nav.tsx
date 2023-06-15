6a1ea08022c4d889d18317fd9d8d40e2c7c57e5e
arihanv <arihanvaranasi@gmail.com>
2023-06-15 01:35:46 -0500

    Made Main Page UI

---
+++ src/components/chat-nav.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Chatnav = () => {
+    return (
+        <div>
+            <div className="h-[8vh] px-5 flex items-center gap-4 bg-gray-900">
+                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+                <div className="border-r border-gray-500 h-[70%]"></div>
+                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Chatnav;
---
+++ src/components/side-bar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SideBar = () => {
+    return (
+        <div className="flex-col flex gap-4 w-fit p-2.5 bg-gray-800 min-w-[240px]">
+            <div className="bg-gray-600 rounded-lg p-2.5">
+                <div className="font-semibold">#cybersecurity #baking</div>
+                <div className="text-sm text-gray-300">3 members</div>
+            </div>
+            <div className="bg-gray-600 rounded-lg p-2.5">
+                <div className="font-semibold">#tennis #gaming</div>
+                <div className="text-sm text-gray-300">5 members</div>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
