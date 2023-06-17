bdd287ab66663f0046631170c2356c3d0adb9c41
JamDon2 <hello@jamdon2.dev>
2023-06-17 19:17:58 +0200

    call callback with validated params in zodWrapper

--- src/util/zodWrapper.ts
+++ src/util/zodWrapper.ts
@@ -9,29 +9,45 @@ export default function zodWrapper<
     searchParams: SearchParams,
     response: Response,
     fn: (
+        body: Zod.infer<Body>,
+        searchParams: Zod.infer<SearchParams>,
         request: NextRequest
     ) => Zod.infer<Response> | Promise<Zod.infer<Response>>
 ) {
     return async (request: NextRequest) => {
-        if (!body.safeParse(request.body).success) {
+        let bodyJSON: Zod.infer<Body> | null;
+
+        try {
+            bodyJSON = JSON.parse(await request.text());
+        } catch {
+            bodyJSON = null;
+        }
+
+        const parsedBody = body.safeParse(bodyJSON);
+
+        if (!parsedBody.success) {
             return NextResponse.json(
                 { error: "Input validation failed" },
                 { status: 400 }
             );
         }
 
-        if (
-            !searchParams.safeParse(
-                Object.fromEntries(new URL(request.url).searchParams.entries())
-            ).success
-        ) {
+        const parsedSearchParams = searchParams.safeParse(
+            Object.fromEntries(new URL(request.url).searchParams.entries())
+        );
+
+        if (!parsedSearchParams.success) {
             return NextResponse.json(
                 { error: "Input validation failed" },
                 { status: 400 }
             );
         }
 
-        const result = await fn(request);
+        const result = await fn(
+            parsedBody.data,
+            parsedSearchParams.data,
+            request
+        );
 
         const parsedResponse = response.safeParse(result);
 
