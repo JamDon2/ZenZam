0603115139d2dab245be16531599a9f93cf0e1b5
JamDon2 <hello@jamdon2.dev>
2023-06-17 23:22:41 +0200

    make navbar functional

--- src/app/chats/[id]/layout.tsx
+++ src/app/chats/[id]/layout.tsx
@@ -2,10 +2,21 @@ import styles from "styles/Home.module.css";
 import React from "react";
 import ChatNav from "components/ChatNav";
 
-export default function Home({ children }: { children: React.ReactNode }) {
+const chatIds = ["1000", "1001", "1002", "1003", "1004"];
+
+export default function Home({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: { id: string };
+}) {
     return (
         <div className={styles.container}>
-            <ChatNav />
+            <ChatNav
+                chatIds={["1000", "1001", "1002", "1003", "1004"]}
+                selected={chatIds.indexOf(params.id)}
+            />
 
             {children}
         </div>
--- src/components/ChatNav.tsx
+++ src/components/ChatNav.tsx
@@ -1,15 +1,34 @@
+import Link from "next/link";
 import React from "react";
 
-function ChatNav() {
+function ChatNav({
+    chatIds,
+    selected,
+}: {
+    chatIds: string[];
+    selected: number;
+}) {
     return (
         <div>
             <div className="h-[8vh] px-5 flex items-center gap-4 bg-gray-800 drop-shadow-md">
-                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+                <div className="rounded-full bg-[#FAFF13] w-10 aspect-square flex justify-center items-center">
+                    <span className="">Z</span>
+                </div>
+
                 <div className="rounded-full bg-gray-500 w-[0.15rem] h-[40%]"></div>
-                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
-                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
-                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
-                <div className="rounded-full bg-yellow-500 w-10 aspect-square"></div>
+
+                {chatIds.map((id, i) => {
+                    return (
+                        <Link href={`/chats/${id}`} key={i}>
+                            <div
+                                className={`rounded-full transition-all bg-yellow-500 w-10 hover:scale-110 aspect-square ${
+                                    selected === i &&
+                                    "outline outline-offset-2 outline-blue-500"
+                                }`}
+                            ></div>
+                        </Link>
+                    );
+                })}
             </div>
         </div>
     );
