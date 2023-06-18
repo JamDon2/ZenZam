14b12ff557806cef407d5b0f8e79cde7bb95bbc8
JamDon2 <hello@jamdon2.dev>
2023-06-18 04:43:22 +0200

    remove space

--- src/app/user/[userId]/chats/[chatId]/layout.tsx
+++ src/app/user/[userId]/chats/[chatId]/layout.tsx
@@ -33,7 +33,6 @@ export default async function ChatLayout({
                 </>
             ) : (
                 <>
-                    {" "}
                     <div className="bg-zinc-950 h-[100vh] flex justify-center items-center">
                         <div className="bg-gray-900 rounded-lg w-full h-full max-w-[450px] max-h-[550px] p-2.5 flex flex-col items-center justify-center gap-2">
                             <span className="text-white font-bold text-2xl">
